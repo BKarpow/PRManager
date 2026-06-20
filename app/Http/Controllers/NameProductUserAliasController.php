1de6846cb348b69fa66dfbe27cb52fec3603d4a5
BKarpow <xymerone@gmail.com>
2026-06-20 18:19:35 +0300

    Make Model and other files

---
+++ app/Http/Controllers/NameProductUserAliasController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NameProductUserAlias;
+use App\Http\Requests\StoreNameProductUserAliasRequest;
+use App\Http\Requests\UpdateNameProductUserAliasRequest;
+
+class NameProductUserAliasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNameProductUserAliasRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NameProductUserAlias $nameProductUserAlias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NameProductUserAlias $nameProductUserAlias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNameProductUserAliasRequest $request, NameProductUserAlias $nameProductUserAlias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NameProductUserAlias $nameProductUserAlias)
+    {
+        //
+    }
+}
